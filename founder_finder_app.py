1da8d20c85f4cb5cb9cdf608034cda6eb48fbc12
Utsavv1 <utsav.gfuturetech@gmail.com>
2025-08-06 17:07:04 +0530

    chnages

--- founder_finder_app.py
+++ founder_finder_app.py
@@ -88,4 +88,6 @@ def download_file():
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
